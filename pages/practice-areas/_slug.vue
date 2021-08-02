e6dad0499f53db072fe139e77b54cbac466fb397
FordanG <60175691+FordanG@users.noreply.github.com>
2021-08-02 12:15:58 +0800

    Updated Practice Area Details View

---
+++ pages/practice-areas/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ info }}</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const info = await $content(`practice-areas`, params.slug).fetch()
+
+    return { info }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
