5fd1ebec645754498bbfbc84ddce3ec66ffe95b9
FordanG <60175691+FordanG@users.noreply.github.com>
2021-08-06 00:13:20 +0800

    Changed the blog card to be dynamic wtih a minimum height

--- components/GBlog.vue
+++ components/GBlog.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="bg-white shadow rounded flex card">
+  <div class="relative overflow-hidden bg-white shadow rounded flex card">
     <div class="w-2/3 flex flex-col justify-between p-12">
       <div>
         <h2 class="text-xl font-semibold text-primary">{{ details.title }}</h2>
         <p>{{ details.description }}</p>
       </div>
-      <div class="flex text-secondary">
+      <div class="flex text-secondary space-x-8">
         <p>{{ details.author }}</p>
-        <p>{{ details.date }}</p>
+        <p>{{ new Date(details.date).toLocaleString() }}</p>
       </div>
     </div>
-    <div>
-      <img :src="details.image" alt="" />
+    <div class="absolute right-0 w-1/3">
+      <img
+        :src="details.image"
+        :alt="details.image"
+        class="w-full object-cover"
+      />
     </div>
   </div>
 </template>
