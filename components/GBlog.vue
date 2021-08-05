663272164e3cc0ac61e784de850a7a0b1da75022
FordanG <60175691+FordanG@users.noreply.github.com>
2021-08-05 20:49:35 +0800

    Add blog; modify blog fields to add author and image

---
+++ components/GBlog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-white shadow rounded flex card">
+    <div class="w-2/3 flex flex-col justify-between p-12">
+      <div>
+        <h2 class="text-xl font-semibold text-primary">{{ details.title }}</h2>
+        <p>{{ details.description }}</p>
+      </div>
+      <div class="flex text-secondary">
+        <p>{{ details.author }}</p>
+        <p>{{ details.date }}</p>
+      </div>
+    </div>
+    <div>
+      <img :src="details.image" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    details: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-height: 15.625rem;
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <g-header heading="G-News" description="Hear about" />
+    <div class="container">
+      <g-blog v-for="blog in blogs" :key="blog.slug" :details="blog" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    // const { lawyersHeader } = await $content('lawyers-page').fetch()
+
+    const blogs = await $content('blog').fetch()
+
+    return { blogs }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
