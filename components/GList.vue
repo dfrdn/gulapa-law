c7216ac9a31a410b96057672a839b3f67522d836
FordanG <60175691+FordanG@users.noreply.github.com>
2021-08-10 23:09:10 +0800

    Add Pullout Component

--- components/GList.vue
+++ components/GList.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="hidden lg:flex lg:w-4/12 lg:justify-end lg:items-start">
+  <div
+    class="
+      overflow-y-auto
+      bg-white
+      lg:h-auto lg:flex lg:w-4/12 lg:justify-end lg:items-start
+    "
+  >
+    <button
+      class="fixed lg:hidden h-10 top-20 right-0 bg-white z-50"
+      @click="handleTable"
+    >
+      pullout
+    </button>
     <div
-      class="lg:block mt-32 w-10/12 align-start bg-white shadow sticky top-32"
+      id="GList"
+      v-if="!isHidden"
+      class="
+        absolute
+        right-0
+        left-10
+        top-0
+        bottom-0
+        lg:block lg:mt-32 lg:w-10/12
+        align-start
+        bg-white
+        shadow
+        lg:sticky lg:top-32
+      "
     >
       <ul class="divide-y divide-gray-300 rounded border">
         <li
@@ -27,6 +52,33 @@ export default {
     contents: {
       type: Array,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isHidden: true,
+    }
+  },
+  mounted() {
+    const windowWidth =
+      window.screen.width < window.outerWidth
+        ? window.screen.width
+        : window.outerWidth
+    this.isHidden = windowWidth < 500
+  },
+  methods: {
+    handleTable() {
+      this.isHidden = !this.isHidden
+      const scrollY = window.scrollY
+      if (!this.isHidden) {
+        document.body.style.top = `-${scrollY}px`
+        // document.getElementById('GList').style.top = `-${scrollY + 62}px`
+        document.body.style.position = 'fixed'
+      } else {
+        document.body.style.position = ''
+        document.body.style.top = ''
+        console.log(scrollY)
+      }
     },
   },
 }
