a466bb032414f0f55bf0661f3c45ef83577c9c47
FordanG <60175691+FordanG@users.noreply.github.com>
2021-08-12 18:52:32 +0800

    Add Icons directory to components

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: ['~/components', '~/components/icons'],
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
