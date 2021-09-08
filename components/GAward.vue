753eb82c995b483fca1376908d66427ccd2c5cfd
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-08 16:23:41 +0800

    Design Awards Page - Mobile and Desktop View

---
+++ components/GAward.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="rounded-lg bg-primary py-5 md:pl-6 px-4 md:px-0">
+      <h2 class="text-white md:text-2xl text-lg font-bold flex">
+        {{ details.awardbody }}
+      </h2>
+    </div>
+    <div class="space-y-2">
+      <p
+        v-for="award in details.award"
+        :key="details.slug"
+        :details="award"
+        class="ml-4 xl:ml-20 xl:text-xl text-md"
+      >
+        {{ `> ${award}` }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    details: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h2:after {
+  content: '';
+  @apply md:border-2 md:border-secondary md:ml-4 md:my-auto md:flex-grow;
+}
+h2:before {
+  content: '';
+  @apply border-2 border-secondary mr-2 md:border-0;
+}
+</style>
--- pages/awards.vue
+++ pages/awards.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>Gains and Recognitions</div>
+  <div>
+    <g-header
+      :heading="awardsHeader.heading"
+      :description="awardsHeader.description"
+    />
+    <div class="container space-y-20 xl:px-0 px-5">
+      <g-award
+        v-for="award in awards"
+        :key="award.slug"
+        :details="award"
+        class="space-y-10"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import GAwards from '~/components/GAward.vue'
 export default Vue.extend({
+  components: { GAwards },
   async asyncData({ $content }) {
-    // const { awardsHeader } = await $content('awards-page').fetch()
-    // return { awardsHeader }
+    const { awardsHeader } = await $content('awards-page').fetch()
+    const awards = await $content('awards').fetch()
+    return { awardsHeader, awards }
   },
 })
 </script>
