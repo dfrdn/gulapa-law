65ddd2293debf62a31ee2b4a519399537a120b02
FordanG <60175691+FordanG@users.noreply.github.com>
2021-11-30 14:24:04 +0800

    update navbar mobile view

--- components/GNavbar.vue
+++ components/GNavbar.vue
@@ -1,6 +1,5 @@
 <template>
   <nav
-    :class="[isExpanded ? 'max-w-full' : 'max-w-1320 md:rounded-lg']"
     class="
       z-50
       shadow-around
@@ -14,9 +13,142 @@
       transition-all
       mx-auto
       w-full
+      max-w-1320
+      md:rounded-lg
     "
   >
-    <div class="container px-2 md:px-6 md:py-4">
+    <div class="container px-5 py-2 md:py-0 md:px-8 lg:px-12 xl:px-0">
+      <div class="flex items-center justify-between">
+        <div
+          class="
+            grid grid-cols-3
+            justify-items-center
+            place-items-center place-content-center
+            md:hidden
+          "
+        >
+          <!-- Mobile toggle -->
+          <div class="md:hidden justify-self-start flex">
+            <button class="items-center" @click="drawer">
+              <svg
+                class="h-8 w-8 fill-current text-primary"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
+          </div>
+
+          <!-- Header logo -->
+          <div class="md:hidden">
+            <n-link :to="links[0].link">
+              <g-logo />
+            </n-link>
+          </div>
+        </div>
+
+        <!-- Navbar -->
+        <div class="hidden md:block container px-2 md:px-6 md:py-4">
+          <div class="hidden md:flex justify-around md:items-center">
+            <n-link v-for="link in links" :key="link.label" :to="link.link">
+              {{ link.label }}
+            </n-link>
+          </div>
+        </div>
+
+        <!-- Dark Background Transition -->
+        <transition
+          enter-class="opacity-0"
+          enter-active-class="ease-out transition-medium"
+          enter-to-class="opacity-100"
+          leave-class="opacity-100"
+          leave-active-class="ease-out transition-medium"
+          leave-to-class="opacity-0"
+        >
+          <div
+            @keydown.esc="isOpen = false"
+            v-show="isOpen"
+            class="z-10 fixed inset-0 transition-opacity"
+          >
+            <div
+              @click="isOpen = false"
+              class="absolute inset-0 bg-black opacity-50"
+              tabindex="0"
+            ></div>
+          </div>
+        </transition>
+
+        <!-- Drawer Menu -->
+        <aside
+          class="
+            px-5
+            py-2
+            transform
+            top-0
+            left-0
+            w-64
+            bg-white
+            fixed
+            h-full
+            overflow-auto
+            ease-in-out
+            transition-all
+            duration-300
+            z-30
+          "
+          :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
+        >
+          <div class="close">
+            <button
+              class="absolute top-0 right-0 mt-4 mr-4"
+              @click="isOpen = false"
+            >
+              <svg
+                class="w-6 h-6 text-primary"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </button>
+          </div>
+
+          <span
+            @click="isOpen = false"
+            class="flex w-full items-center p-4 border-b text-primary"
+          >
+            <n-link :to="links[0].link">
+              <g-logo class="h-12" />
+            </n-link>
+          </span>
+
+          <ul
+            class="tracking-wider text-primary text-lg"
+            @click="isOpen = false"
+          >
+            <n-link
+              v-for="link in links"
+              :key="link.label"
+              :to="link.link"
+              class="block border-t py-4"
+            >
+              {{ link.label }}
+            </n-link>
+          </ul>
+        </aside>
+      </div>
+    </div>
+
+    <!-- <div class="container px-2 md:px-6 md:py-4">
       <div class="hidden md:flex justify-around md:items-center">
         <n-link :to="links[0].link">
           <span v-if="!isExpanded">{{ links[0].label }}</span>
@@ -45,7 +177,7 @@
         />
         <g-logo class="place-self-center" />
       </div>
-    </div>
+    </div> -->
   </nav>
 </template>
 
@@ -55,6 +187,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
+      isOpen: false,
       links: [
         {
           label: 'G-Law',
@@ -85,34 +218,50 @@ export default Vue.extend({
           link: '/blog',
         },
       ],
-      isExpanded: false,
-      scrollPosition: 0,
-      burgerStyle: {
-        '--padding': '10px',
-        '--layer-bg-color': 'yellow',
-        '--active-layer-bg-color': '#ABABAB',
-        '--layer-width': '10px',
-      },
+      // isExpanded: false,
+      // scrollPosition: 0,
+      // burgerStyle: {
+      //   '--padding': '10px',
+      //   '--layer-bg-color': 'yellow',
+      //   '--active-layer-bg-color': '#ABABAB',
+      //   '--layer-width': '10px',
+      // },
     }
   },
-  beforeMount() {
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  beforeDestroy() {
-    window.removeEventListener('scroll', this.handleScroll)
-  },
+  // beforeMount() {
+  //   window.addEventListener('scroll', this.handleScroll)
+  // },
+  // beforeDestroy() {
+  //   window.removeEventListener('scroll', this.handleScroll)
+  // },
+  // methods: {
+  //   handleScroll() {
+  //     const width =
+  //       window.innerWidth > 0
+  //         ? window.innerWidth
+  //         : document.documentElement.clientWidth
+  //     this.scrollPosition = window.scrollY
+  //     if (this.scrollPosition >= 120) {
+  //       this.isExpanded = true
+  //     } else {
+  //       this.isExpanded = false
+  //     }
+  //   },
+  // },
   methods: {
-    handleScroll() {
-      const width =
-        window.innerWidth > 0
-          ? window.innerWidth
-          : document.documentElement.clientWidth
-      this.scrollPosition = window.scrollY
-      if (this.scrollPosition >= 120) {
-        this.isExpanded = true
-      } else {
-        this.isExpanded = false
-      }
+    drawer() {
+      this.isOpen = !this.isOpen
+    },
+  },
+  watch: {
+    isOpen: {
+      immediate: true,
+      handler(isOpen) {
+        if (process.client) {
+          if (isOpen) document.body.style.setProperty('overflow', 'hidden')
+          else document.body.style.removeProperty('overflow')
+        }
+      },
     },
   },
 })
