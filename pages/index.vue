06a3e36021ce875303d5fece96fb9fabd866055e
FordanG <60175691+FordanG@users.noreply.github.com>
2022-04-14 21:02:26 +0800

    remove lawyers image homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,7 @@
       <div class="flex w-full items-center px-16 md:px-0">
         <hr class="hidden md:block" />
         <h1
-          class="
-            text-primary
-            font-bold
-            text-2xl
-            md:text-4xl
-            text-center
-            md:mx-2
-          "
+          class="text-primary font-bold text-2xl md:text-4xl text-center md:mx-2"
         >
           {{ hero.heading }}
         </h1>
@@ -38,30 +31,12 @@
       </div>
       <div class="px-2">
         <div
-          class="
-            bg-white
-            rounded-lg
-            container
-            shadow
-            flex flex-col
-            md:flex-row
-            transform
-            translate-y-1/2
-          "
+          class="bg-white rounded-lg container shadow flex flex-col md:flex-row transform translate-y-1/2"
         >
           <div
             v-for="point in hero.points"
             :key="point.heading"
-            class="
-              flex
-              space-x-4
-              py-8
-              px-4
-              border-b
-              last:border-b-0
-              md:border-b-0 md:border-r md:last:border-r-0
-              justify-center
-            "
+            class="flex space-x-4 py-8 px-4 border-b last:border-b-0 md:border-b-0 md:border-r md:last:border-r-0 justify-center"
           >
             <img :src="point.icon" :alt="point.heading" class="block w-1/6" />
             <div class="w-4/6 flex flex-col justify-center">
@@ -87,16 +62,7 @@
               <div class="mx-8 md:mx-0 md:ml-8">
                 <p class="my-8 text-sm md:text-base">{{ about.description }}</p>
                 <n-link
-                  class="
-                    bg-secondary
-                    rounded
-                    px-10
-                    py-3
-                    text-sm
-                    inline-flex
-                    items-center
-                    capitalize
-                  "
+                  class="bg-secondary rounded px-10 py-3 text-sm inline-flex items-center capitalize"
                   to="/about"
                 >
                   Learn more
@@ -117,46 +83,17 @@
     <section id="offices" class="container">
       <g-title :heading="offices.heading" :subheading="offices.subheading" />
       <p
-        class="
-          text-center
-          md:text-left
-          mx-4
-          md:mx-0 md:ml-8
-          my-8
-          text-sm
-          md:text-base
-        "
+        class="text-center md:text-left mx-4 md:mx-0 md:ml-8 my-8 text-sm md:text-base"
       >
         {{ offices.description }}
       </p>
       <div
-        class="
-          flex flex-col
-          xl:gap-18
-          gap-10
-          md:grid md:grid-auto-flow
-          lg:grid-cols-4
-          md:grid-cols-2
-          sm:grid-cols-1
-          xl:px-0
-          px-5
-          items-center
-          content-around
-        "
+        class="flex flex-col xl:gap-18 gap-10 md:grid md:grid-auto-flow lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 xl:px-0 px-5 items-center content-around"
       >
         <n-link
           v-for="location in officeLocations"
           :key="location.location"
-          class="
-            place-self-center
-            rounded
-            overflow-hidden
-            relative
-            w-60
-            h-60
-            bg-primary
-            text-center
-          "
+          class="place-self-center rounded overflow-hidden relative w-60 h-60 bg-primary text-center"
           :to="location.path"
         >
           <img
@@ -165,16 +102,7 @@
             class="h-full object-cover opacity-30"
           />
           <h3
-            class="
-              absolute
-              top-1/2
-              left-1/2
-              transform
-              -translate-x-1/2 -translate-y-1/2
-              text-white
-              font-bold
-              text-2xl
-            "
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-2xl"
           >
             {{ location.location }}
           </h3>
@@ -195,17 +123,7 @@
               </p>
 
               <n-link
-                class="
-                  bg-secondary
-                  rounded
-                  px-10
-                  py-3
-                  text-sm
-                  hidden
-                  md:inline-flex
-                  items-center
-                  capitalize
-                "
+                class="bg-secondary rounded px-10 py-3 text-sm hidden md:inline-flex items-center capitalize"
                 to="/practice-areas"
               >
                 See all practice areas
@@ -216,20 +134,7 @@
           <div class="md:w-2/3">
             <div
               id="practices"
-              class="
-                xl:gap-18
-                gap-10
-                md:grid md:grid-auto-flow
-                flex
-                overflow-x-auto
-                md:overflow-visible
-                lg:grid-cols-3
-                md:grid-cols-2
-                sm:grid-cols-1
-                xl:px-0
-                px-5
-                md:ml-0
-              "
+              class="xl:gap-18 gap-10 md:grid md:grid-auto-flow flex overflow-x-auto md:overflow-visible lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xl:px-0 px-5 md:ml-0"
             >
               <g-practice
                 v-for="practice in practiceAreas"
@@ -241,19 +146,7 @@
             </div>
           </div>
           <n-link
-            class="
-              bg-secondary
-              rounded
-              px-10
-              py-3
-              mt-8
-              text-sm
-              md:hidden
-              inline-flex
-              items-center
-              capitalize
-              mx-auto
-            "
+            class="bg-secondary rounded px-10 py-3 mt-8 text-sm md:hidden inline-flex items-center capitalize mx-auto"
             to="/practice-areas"
           >
             See all practice areas
@@ -279,20 +172,14 @@
               >
                 <n-link
                   :to="featuredLawyers[lawyers.lawyers.indexOf(lawyer)].path"
-                  class="
-                    flex
-                    items-center
-                    text-secondary
-                    uppercase
-                    font-semibold
-                  "
+                  class="flex items-center text-secondary uppercase font-semibold"
                 >
                   <img
                     :src="
                       featuredLawyers[lawyers.lawyers.indexOf(lawyer)].image
                     "
                     :alt="featuredLawyers[lawyers.lawyers.indexOf(lawyer)].name"
-                    class="h-8 w-8 rounded-full object-fill mr-2 md:hidden" />{{
+                    class="h-8 w-8 rounded-full object-fill mr-2" />{{
                     lawyer.name
                   }}<chevron-icon class="w-5 transform"
                 /></n-link>
@@ -300,14 +187,7 @@
                   {{ lawyer.description }}
                 </p>
                 <div
-                  class="
-                    flex flex-col
-                    md:flex-row
-                    space-y-2
-                    md:space-y-0
-                    justify-between
-                    text-sm
-                  "
+                  class="flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between text-sm"
                 >
                   <a :href="`mailto:${lawyer.email}`" class="flex space-x-2">
                     <email-icon class="w-5 fill-current text-primary" />
@@ -327,17 +207,7 @@
 
             <div class="flex justify-center">
               <n-link
-                class="
-                  bg-primary
-                  rounded
-                  px-10
-                  py-3
-                  text-sm
-                  inline-flex
-                  items-center
-                  capitalize
-                  text-white
-                "
+                class="bg-primary rounded px-10 py-3 text-sm inline-flex items-center capitalize text-white"
                 to="/lawyers"
               >
                 See all our lawyers
@@ -346,32 +216,21 @@
             </div>
           </div>
         </div>
-        <div class="md:block hidden">
+        <!-- <div class="md:block hidden">
           <img :src="lawyers.image" :alt="lawyers.image" />
-        </div>
+        </div> -->
       </div>
     </section>
     <section id="awards" class="bg-primary -mb-16">
       <div class="container flex flex-col items-center space-y-8">
         <h2 class="text-white text-2xl font-semibold">{{ awards.heading }}</h2>
         <div
-          class="
-            flex flex-col
-            md:flex-row md:justify-between md:space-x-16
-            w-full
-          "
+          class="flex flex-col md:flex-row md:justify-between md:space-x-16 w-full"
         >
           <div
             v-for="award in awards.featured"
             :key="award.award"
-            class="
-              text-center
-              md:w-1/3 md:space-y-8
-              space-y-6
-              flex flex-col
-              items-center
-              relative
-            "
+            class="text-center md:w-1/3 md:space-y-8 space-y-6 flex flex-col items-center relative"
           >
             <img
               src="~assets/img/award-home.svg"
@@ -392,17 +251,7 @@
           </div>
         </div>
         <n-link
-          class="
-            bg-secondary
-            rounded
-            px-10
-            py-3
-            text-sm
-            inline-flex
-            items-center
-            capitalize
-            text-white
-          "
+          class="bg-secondary rounded px-10 py-3 text-sm inline-flex items-center capitalize text-white"
           to="/awards"
         >
           See all gains and recognitions
