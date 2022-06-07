0c5db1c6847fe2b7f8c4fa2dfb5f3da098a8d537
FordanG <60175691+FordanG@users.noreply.github.com>
2022-06-08 04:57:57 +0800

    update metatags

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,32 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+
+      // <!-- Open Graph / Facebook -->
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: 'Gulapa Law' },
+      {
+        property: 'og:description',
+        content:
+          'Gulapa Law has extensive and significant experience in various legal fields. Its practice area includes general corporate, mergers and acquisitions (M&A), projects and infrastructure (such as Public-Private Partnership projects), banking and finance, real estate and construction, technology, media and telecommunications (TMT), antitrust and competition, intellectual property, and dispute resolution.',
+      },
+      {
+        property: 'og:image',
+        content: 'https://glaw.netlify.app/gulapalaw-meta.jpg',
+      },
+
+      // <!-- Open Graph / Twitter -->
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:title', content: 'Gulapa Law' },
+      {
+        property: 'twitter:description',
+        content:
+          'Gulapa Law has extensive and significant experience in various legal fields. Its practice area includes general corporate, mergers and acquisitions (M&A), projects and infrastructure (such as Public-Private Partnership projects), banking and finance, real estate and construction, technology, media and telecommunications (TMT), antitrust and competition, intellectual property, and dispute resolution.',
+      },
+      {
+        property: 'twitter:image',
+        content: 'https://glaw.netlify.app/gulapalaw-meta.jpg',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
